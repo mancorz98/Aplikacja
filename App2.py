bd6a46237065c3a313d523073d69eb5d31cc7ceb
MSI <karolbednarz02@gmail.com>
2023-03-18 14:09:03 +0100

    Dzialajaca apka

--- App2.py
+++ App2.py
@@ -20,6 +20,11 @@ import matplotlib.pyplot as plt
 import _osx_support
 from kivy.clock import Clock
 
+import pandas as pd
+import seaborn as sns
+
+from MemProgrammer import MemProgrammer
+import plyer
 import numpy as np
 class FileChoosePopup(Popup):
     load = ObjectProperty(None)
@@ -27,31 +32,36 @@ class FileChoosePopup(Popup):
     def __init__(self,**kwargs):
         #execute the normal __init__ from the parent
         super().__init__(**kwargs)
-        self.ids.filechooser = os.getcwd()
+        self.ids.filechooser.path = os.getcwd()
 
 class FigurePopup(Popup):
-    load = ObjectProperty(None)
     path = ObjectProperty(None)
+    file_path = ObjectProperty(None)
     def __init__(self,**kwargs):
         #execute the normal __init__ from the parent
         super().__init__(**kwargs)
-        Clock.schedule_interval(self.on_start,1)
+        self.plot()
         
         
     def plot(self):
-        signal = [7, 89.6, 45.-56.34]
+        data = pd.read_csv(self.file_path)
+        print(self.file_path)
 
-        signal = np.array(signal)
+        data.columns = ["Time", "Pulses", "Tests",'R','isSucces','dt_Ron',
+                        'Amp_Ron', 'q', 'E_memristor', 'State']
 
-        # this will plot the signal on graph
-        plt.plot(signal)
-        # setting x label
-        plt.xlabel('Time(s)')
+        temp_data = data[data["isSucces"]==True].groupby(by="Pulses").count()
+        plt.figure(figsize=(10,5))
+        sns.barplot( x=temp_data.index, y="R", data=temp_data)
+        plt.xlabel("$No.\ of\ Pulses$", fontsize=15)
+        plt.ylabel("Number of successful tests to $R_{on}$ state ", fontsize=15)
+        plt.grid(True)
+        
+        box = self.ids.Ploting_box
+        box.clear_widgets()
+        self.figure = plt.gcf()
+        box.add_widget(FigureCanvasKivyAgg(  self.figure))
 
-        # setting y label
-        plt.ylabel('signal (norm)')
-        plt.grid(True, color='lightgray')
-        plt.show()
         
     def on_start(self,*args):
         signal = [7, 89.6, 45., -56.34]
@@ -85,8 +95,11 @@ class MainPanel(FloatLayout):
     plot_popup = ObjectProperty(None)
 
     def open_popup(self):
+
         self.the_popup = FileChoosePopup(load = self.load)
         self.the_popup.open()
+      
+        
 
     def load(self, selection):
         self.file_path = str(selection)
@@ -99,18 +112,26 @@ class MainPanel(FloatLayout):
             self.ids.FilePath.color = "white"
 
     def startMeasure(self):
-        print("Hello World")
-        self.directory = self.ids.FilePath.text
-        self.Amp_On = round(self.ids.AmpOn.value,1)
-        self.dt_Ron = self.ids.dtOn.value/1000.
-        print(self.Amp_On)
-        #print(self.ids.pos_hint)
-        
-        if os.path.isdir(self.directory):
-            self.make_file()
-        else:
+        if not os.path.isdir(self.file_path):
             self.ids.FilePath.color = "red"
             self.ids.FilePath.text = "Nie wybrano ścieżki"
+        else:
+            print("Hello World")
+            self.directory = self.ids.FilePath.text
+            self.Amp_On = round(self.ids.AmpOn.value,1)
+            self.dt_Ron = self.ids.dtOn.value/1000.
+            self.max_tests = int(self.ids.max_test.text)
+            self.max_pulses = int(self.ids.max_pulses.text)
+            
+            print(self.Amp_On)
+            #print(self.ids.pos_hint)
+            programmer = MemProgrammer(device_name = "myDAQ1",fs_acq=10000. , N = 10000,r = 4.7, states_limit= (4, 50))
+            self.measure_file = programmer.setting_Ron_measurment(n_mem=1,Amp_On=self.Amp_On, Amp_Off=-1.5, dt_On=self.dt_Ron, dt_Off=0.1,
+                                        max_tests=self.max_tests, max_pulses=self.max_pulses, saving=True,directory=self.directory)
+            programmer.closing()
+            self.ids.plot.disabled = False
+            
+
 
 
 
@@ -124,13 +145,10 @@ class MainPanel(FloatLayout):
             print("Operacja udana")
 
     def open_popup_plot(self):
-        self.plot_popup = FigurePopup(path = self.file_path)
+        self.plot_popup = FigurePopup(path = self.file_path, file_path = self.measure_file)
         self.plot_popup.open()
         
         
-        
-        
-
 
 
 
